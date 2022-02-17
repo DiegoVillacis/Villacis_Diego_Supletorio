e84db40aa9b1dac7134575117a7b6071dffd42b4
DiegoVillacis <dvillacis2229@uta.edu.ec>
2022-02-16 19:04:00 -0500

    Commit 7h00

--- app/src/main/java/com/deitel/villacis_diego_supletorio/MainActivity.java
+++ app/src/main/java/com/deitel/villacis_diego_supletorio/MainActivity.java
@@ -1,20 +1,86 @@
 package com.deitel.villacis_diego_supletorio;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText correo,contraseña;
+    String textCorreo,textContrasenia;
+    Button aceptar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        correo = findViewById(R.id.editText_correo);
+        contraseña = findViewById(R.id.editText_contrasenia);
+        aceptar = findViewById(R.id.button_iniciar_sesion);
+
+        aceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textCorreo = correo.getText().toString();
+                textContrasenia = contraseña.getText().toString();
+                if(!Patterns.EMAIL_ADDRESS.matcher(textCorreo).matches()){
+                    correo.setError("Verfifica que el correo esté bien escrito");
+                    correo.requestFocus();
+                }else if (textContrasenia.isEmpty()){
+                    contraseña.setError("Intente ingresar su contraseña");
+                    contraseña.requestFocus();
+                }else{
+                    IngresarFirebase(textCorreo,textContrasenia);
+                }
+            }
+
+
+        });
     }
 
+    private void IngresarFirebase(String correob,String contraseñab) {
+        Toast.makeText(MainActivity.this, textCorreo+textContrasenia,
+                Toast.LENGTH_SHORT).show();
 
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
 
+                            Toast.makeText(MainActivity.this, "Authentication susces.",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
 
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        UsuarioLogeado();
+    }
+    private void UsuarioLogeado(){
+        if(mAuth != null){
+            //Toast.makeText(MenuPrincipal.this, "Usuario logeado", Toast.LENGTH_SHORT).show();
+        }else{
+            startActivity(new Intent(MainActivity.this,MainActivity2.class));
+            finish();
+        }
+    }
 
 }
